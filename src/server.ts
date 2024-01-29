bd93ffd472d435813ee123530327b225c4582439
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2024-01-29 20:54:25 -0300

    up api

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de teste',
+    amount: 1000,
+  }).returning('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server up!')
+  })
