c58024331af16d64e753f18675aebbb35127863e
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2024-02-05 20:01:02 -0300

    feat: import middleware to verify session id exist

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,11 +2,17 @@ import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { knex } from "../database"
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExist } from "../middlewares/check-session-id-exist"
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
-    app.get('/', async () => {
-        const transactions = await knex('transactions').select()
+    app.get('/', {preHandler: [checkSessionIdExist]}, async (request) => {
+
+        const {sessionId} = request.cookies
+
+        const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
         return {
             transactions
@@ -15,22 +21,29 @@ export async function transactionsRoutes(app: FastifyInstance) {
         // return transactions
     })
 
-    app.get('/:id', async (request) => {
+    app.get('/:id', {preHandler: [checkSessionIdExist]}, async (request) => {
         const getTransactionParamsSchema = z.object({
             id: z.string().uuid(),
         })
 
         const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const { sessionId } = request.cookies
         
-        const transaction = await knex('transactions').where('id', id).first()
+        const transaction = await knex('transactions').where({
+            session_id: sessionId,
+            id,
+        }).first()
 
         return {
             transaction
         }
     })
 
-    app.get('/summary', async () => {
-        const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+    app.get('/summary', {preHandler: [checkSessionIdExist]}, async (request) => {
+        const { sessionId } = request.cookies
+
+        const summary = await knex('transactions').sum('amount', { as: 'amount' }).where('session_id', sessionId).first()
 
         return { summary }
     })
